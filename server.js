5e0718bec096224fa07cec2147d83144e7c1eac1
diana maina <dianamainachui@gmail.com>
2020-11-15 17:39:00 -0500

    added jsx file

--- server.js
+++ server.js
@@ -3,13 +3,23 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("client"))
+// const db = require("./models");
+const AuthController = require("./contrller/authcontroller");
+const Bookocntrolller = require("./controllers/bookcontroller");
+const Athouhtcontroller = require("./controllers/authorController");
+const UserController = require("./controllers/userController");
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
